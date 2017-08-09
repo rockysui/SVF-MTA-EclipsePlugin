2dfa6af1aeb546e6c3a902516cc03148cb18429f
Vi <vi_nguyen101@hotmail.com>
2017-08-10 01:51:25 +1000

    Can read files contents + some simple annotations from files albeit very buggy

--- svfmtaplugin/src/svfmtaplugin/builder/SampleBuilder.java
+++ svfmtaplugin/src/svfmtaplugin/builder/SampleBuilder.java
@@ -1,6 +1,11 @@
 package svfmtaplugin.builder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -30,22 +35,25 @@ public class SampleBuilder extends IncrementalProjectBuilder {
 		 */
 		public boolean visit(IResourceDelta delta) throws CoreException {
 			IResource resource = delta.getResource();
-
-
+System.out.println("Im reading err in visit with cases");
+			//readErrors();
 			switch (delta.getKind()) {
 			case IResourceDelta.ADDED:
 				// handle added resource
 				//checkXML(resource);
-				checkwords(resource);
-
+//				checkwords(resource);
+				//readErrors();
+				annotateErrors(resource);
 				break;
 			case IResourceDelta.REMOVED:
 				// handle removed resource
+				deleteMarkers((IFile) resource);
 				break;
 			case IResourceDelta.CHANGED:
 				// handle changed resource
 				//checkXML(resource);
-				checkwords(resource);
+			//	checkwords(resource);
+			//	annotateErrors(resource);
 				break;
 			}
 			//return true to continue visiting children.
@@ -56,7 +64,10 @@ public class SampleBuilder extends IncrementalProjectBuilder {
 	class SampleResourceVisitor implements IResourceVisitor {
 		public boolean visit(IResource resource) {
 			//checkXML(resource);
-			checkwords(resource);
+			//checkwords(resource);
+			System.out.println("Im reading err in resource visitor");
+
+			readErrors();
 			//return true to continue visiting children.
 			return true;
 		}
@@ -184,7 +195,71 @@ System.out.println("sadadasd");
 	
 	
 	/////////////////////////////////!@#!@#!@#!@#!@#!@#!#!#
-	//insert code below to detect errors from error file maybs
+	ArrayList<ErrorContainer> ecs = new ArrayList<ErrorContainer>();
+
+	
+	//create class for the population of errors
+	class ErrorContainer {
+		public String firstFile;
+		public String secondFile;
+		public int	  firstErrLine;
+		public int    secondErrLine;
+		public ErrorContainer() {
+		}
+		public ErrorContainer(String s1, String s2, int err1, int err2) {
+			firstFile = s1;
+			secondFile = s2;
+			firstErrLine = err1;
+			secondErrLine = err2;
+		}
+		
+		void setFirstFile(String s) {
+			firstFile = s;
+		}
+	
+		void setSecondFile(String s) {
+			secondFile = s;
+		}
+		
+		void setFirstErrLine(int x) {
+			firstErrLine = x;
+		}
+		
+		void setSecondErrLine(int x) {
+			secondErrLine = x;
+		}
+		
+		String getFirstFile() {
+			return firstFile;
+		}
+		
+		String getSecondFile() {
+			return firstFile;
+		}
+		
+		int getFirstErr() {
+			return firstErrLine;
+		}
+		
+		int getSecondErr() {
+			return secondErrLine;
+		}
+		
+		@Override
+		public boolean equals(Object o) {	
+			if(o == null) return false;
+			if(!(o instanceof ErrorContainer)) return false;	
+			ErrorContainer ec = (ErrorContainer) o;
+			if(ec.firstFile == this.firstFile && ec.secondFile == this.secondFile
+					&& ec.firstErrLine == this.firstErrLine && ec.secondErrLine == this.secondErrLine) return true;
+			
+			
+			
+			return false;
+		}
+	}
+	
+	//insert code below to annotate errors from error file maybs
 	//TODO
 	void checkwords(IResource resource) {
 		if (resource instanceof IFile && resource.getName().endsWith(".java")) {
@@ -207,9 +282,57 @@ System.out.println("sadadasd");
 //
 	}
 	
+	void annotateErrors(IResource resource) {
+		System.out.println("asdasd");
+		for(ErrorContainer ec : ecs) {
+			//this is testing java style programs, so remove the string src/asd (assuming that output and cpp files are in the same thing
+			IFile firstIfile = this.getProject().getFile("src/asd/" + ec.getFirstFile());
+			IFile secondIfile = this.getProject().getFile("src/asd/" + ec.getSecondFile());
+			//deleteMarkers(firstIfile);
+			//deleteMarkers(secondIfile);
+			String fileName = firstIfile.getLocation().toString();
+			System.out.println("first = " + fileName);
+			String sfileName = firstIfile.getLocation().toString();
+			System.out.println("second = " + sfileName);
+			addMarker(firstIfile, "testing", ec.getFirstErr(), 2);
+			addMarker(secondIfile, "linked", ec.getSecondErr(), 2);
+
+		}
+	}
 	//need to read file somehow.
 	//TODO 
 	void readErrors() {
-		
+		try {
+			//gets the output.txt from the java project, regardless if it exists
+			IFile ifile = this.getProject().getFile("output.txt");
+			
+			//gets the string of the path
+			String fileName = ifile.getLocation().toString();
+			//creates a file class
+			File file = new File(fileName);
+			
+			//now does the check if it exists
+			if(!file.exists()) {
+				//do nothing cause file doesn't exist
+				System.out.println("Im here, but should not be");
+				return;
+			}
+			//allows scanner to read from the file
+			Scanner sc = new Scanner(new FileReader(file));
+			System.out.println("I don't need to read this anymore");
+			while(sc.hasNext()) {
+				String err1 = sc.nextLine();
+				String s1 = sc.nextLine();
+				String err2 = sc.nextLine();
+				String s2 = sc.nextLine();
+				
+				ErrorContainer e = new ErrorContainer(s1, s2, Integer.parseInt(err1), Integer.parseInt(err2));
+				ecs.add(e);
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
